1c14c74a8fb79b5b4b8a831f2a3bfefd06c8e82e
Hanna Kalinowska <hanna.kalinowska@simplybusiness.co.uk>
2018-07-16 15:41:02 +0100

    Exercise 5

--- functions/get-index.js
+++ functions/get-index.js
@@ -7,6 +7,10 @@ const URL = require('url')
 const restaurantsApiRoot = process.env.restaurants_api
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+const awsRegion = process.env.AWS_REGION
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
+
 let html
 
 function loadHtml () {
@@ -41,7 +45,15 @@ module.exports.handler = async (event, context) => {
   const template = loadHtml()
   const restaurants = await getRestaurants()
   const dayOfWeek = days[new Date().getDay()]
-  const html = Mustache.render(template, { dayOfWeek, restaurants })
+  const view = {
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    dayOfWeek,
+    restaurants,
+    searchUrl: `${restaurantsApiRoot}/search`
+  }
+  const html = Mustache.render(template, view)
   const response = {
     statusCode: 200,
     headers: {
